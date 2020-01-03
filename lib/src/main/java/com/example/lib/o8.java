7979bba70405541d773be58dbca7cf062309e4ac
ffrank2589 <ffrank2589@yahoo.com.tw>
2020-01-03 14:38:47 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/o8.java
@@ -0,0 +1,10 @@
+package com.example.lib;
+
+import java.util.Scanner;
+
+public class o8 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        CppBank cppBank = new CppBank(Integer.parseInt(scanner.next()),Integer.parseInt(scanner.next()));
+    }
+}
